29b1af67c8409eeacc899f8635dd09e3c68c3f17
luciferinfery <rudfedor@gmail.com>
2023-12-14 14:57:52 +0200

    Add Readme.md

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+let admin;
+let name = "John";
+ admin = name
+alert(admin)
+
+let planetName = "Earth"
+let userName = "Rud Fedir"
+
+let a = "10"
+let b = 10
+let result = a + b
+console.log(typeof result)

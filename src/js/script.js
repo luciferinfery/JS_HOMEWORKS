175631912e7400be86a566c595bc267762a9667d
luciferinfery <rudfedor@gmail.com>
2023-12-14 15:56:02 +0200

    HW_3 complited

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const operand1 = Number(prompt("Enter the first operand: "));
+const operand2 = Number(prompt("Enter the second operand: "));
+
+const sum = operand1 + operand2;
+console.log(`Результат: ${operand1} + ${operand2} = ${sum}`);
+
+const minus = operand1 - operand2;
+console.log(`Результат: ${operand1} - ${operand2} = ${minus}`);
+
+if (operand2 !== 0) {
+    const divide = operand1 / operand2;
+    console.log(`Результат: ${operand1} / ${operand2} = ${divide}`);
+} else {
+    console.log("Error: You cannot divide by 0");
+}
+
+const multiplication = operand1 * operand2;
+console.log(`Результат: ${operand1} * ${operand2} = ${multiplication}`);
+

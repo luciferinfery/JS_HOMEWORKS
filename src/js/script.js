7ad70bb66a98ca2572741e0aaf9256ec1b3c981d
luciferinfery <rudfedor@gmail.com>
2023-12-19 20:37:25 +0200

    HW_4

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const firstString = prompt("Enter first string");
+const secondString = prompt("Enter second string");
+const thirdString = prompt("Enter third string");
+
+console.log(firstString + " " + secondString + " " + thirdString);
+
+const bigNumber = 12345;
+const bigNumberAsString = bigNumber.toString();
+
+console.log(bigNumberAsString[0], bigNumberAsString[1], bigNumberAsString[2], bigNumberAsString[3], bigNumberAsString[4]);

66cfc785fc66e143af6a82037607e046aaf5189b
luciferinfery <rudfedor@gmail.com>
2024-01-02 17:02:18 +0200

    HW_15

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const a = Number(prompt('Enter first number'));
+const b = Number(prompt('Enter second number'));
+const c = Number(prompt('Enter third number'));
+
+const avg = (a + b + c) / 3;
+alert('Average: ' + avg);
+

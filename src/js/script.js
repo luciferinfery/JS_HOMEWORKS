1c7d2e76bd66713e1e94236aa7d2de1a02720cbd
luciferinfery <rudfedor@gmail.com>
2024-01-02 18:21:53 +0200

    HW_12

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const a = parseInt(prompt('Enter the first number'));
+const b = parseInt(prompt('Enter the second number'));
+
+alert(
+    `${a} + ${b} = ${a + b}\n` +
+    `${a} - ${b} = ${a - b}\n` +
+    `${a} * ${b} = ${a * b}\n` +
+    `${a} / ${b} = ${a / b}`
+)

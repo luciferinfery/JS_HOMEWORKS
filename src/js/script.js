cd58b565e0ac8fcf1937d480474905c15a485625
luciferinfery <rudfedor@gmail.com>
2024-01-07 22:29:26 +0200

    HW_20

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const age = prompt('Введіть ваш вік');
+
+if (age && +age) {
+    const lastDigit = +age[age.length - 1];
+    const lastTwoDigits = +(age[age.length - 2] + age[age.length - 1]);
+
+    let nameAge;
+    if (lastTwoDigits && lastTwoDigits >= 11 && lastTwoDigits <= 15) {
+        nameAge = 'років';
+    } else if (lastDigit === 1) {
+        nameAge = 'рік';
+    } else if (lastDigit >= 2 && lastDigit <= 4) {
+        nameAge = 'роки';
+    } else if (lastDigit >= 5 && lastDigit <= 9 || lastDigit === 0) {
+        nameAge = 'років';
+    }
+    console.log(age, nameAge);
+
+} else {
+    console.log('Invalid number!')
+}

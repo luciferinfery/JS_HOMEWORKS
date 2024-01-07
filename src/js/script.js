946ca6961d35111590015ed44e978f95c1a0a4b7
luciferinfery <rudfedor@gmail.com>
2024-01-07 16:28:39 +0200

    HW_17

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+// Переписати код нижче з використанням конструкції switch…case
+
+const numOrStr = prompt('input number or string');
+
+switch (true) {
+    case numOrStr === null:
+        console.log('Ви скасували');
+        break;
+    case numOrStr.trim() === '':
+        console.log('Empty String');
+        break;
+    case isNaN(+numOrStr):
+        console.log('number is Ba_NaN');
+        break;
+    default:
+        console.log('OK!');
+}

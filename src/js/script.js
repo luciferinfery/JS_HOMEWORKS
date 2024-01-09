6fdc73aee12d971019b2690f927544c5cca51043
luciferinfery <rudfedor@gmail.com>
2024-01-09 22:02:51 +0200

    HW_25

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+let numSum = 0;
+loop:
+    for (let num = 1; num <= 20; num++) {
+        if (num % 2 === 0) {
+            continue loop;
+        }
+
+        numSum = numSum + num;
+    }
+console.log(numSum);

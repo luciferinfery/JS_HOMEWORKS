edd5c139811d7a906295cf3ed5edba7265a70b15
luciferinfery <rudfedor@gmail.com>
2024-01-09 19:40:54 +0200

    HW_22 started

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+let result;
+for(let num = 20; num <= 30; num += 0.5) {
+    if (!result) {
+        result = String(num);
+    } else {
+        result = `${result} ${num}`;
+    }
+}
+console.log(result)

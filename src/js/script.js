4c6ecba54ce5d54aecfc6b9c0ab53e57f79ab6ad
luciferinfery <rudfedor@gmail.com>
2024-01-09 22:42:05 +0200

    HW_24

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const num = +prompt('Enter a number');
+
+let resultNum = NaN;
+
+for (let i = 2; i <= num; i++) {
+    if (num % i === 0) {
+        resultNum = i;
+        break;
+    }
+}
+
+console.log(resultNum);

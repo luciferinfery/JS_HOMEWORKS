61a1685e9fec594163bd6b6e7517d85b9eed1ad2
luciferinfery <rudfedor@gmail.com>
2024-01-09 23:15:08 +0200

    HW_23

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const arr = [1, 2, 3, 4, 5];
+
+let sum = 0;
+let sumPow = 0;
+
+for (const value of arr) {
+    sum = sum + value;
+    sumPow = sumPow + value ** 2;
+}
+
+console.log('сума елементів', sum);
+console.log('сума квадратів елементів', sumPow);

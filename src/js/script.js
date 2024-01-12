045eb74e0478525bcb2f6e3b24843657c8aa4434
luciferinfery <rudfedor@gmail.com>
2024-01-12 20:26:03 +0200

    HW_26

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+function generateKey(length, characters) {
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.round(Math.random() * (characters.length - 1));
+        result = result + characters[randomIndex];
+    }
+    return result;
+}
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const key = generateKey(16, characters);
+console.log(key);

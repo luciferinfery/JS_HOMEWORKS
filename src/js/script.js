f3d3733d44d37fecf28ddc0abf61b4a3c16cff42
luciferinfery <rudfedor@gmail.com>
2024-01-12 21:18:22 +0200

    HW_27

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const num = +prompt('Enter the number');
+const power = +prompt('Enter power of number');
+
+function pow(num, power = 1) {
+    if (isNaN(num) || isNaN(power)) {
+        return 'Error';
+    }
+    return num ** power;
+}
+
+let result = pow(num, power);
+alert('Результат в степині:', result);

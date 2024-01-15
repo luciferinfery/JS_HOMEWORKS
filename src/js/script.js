7a97f2842ab9bf907252a1c2257e000c53119b3b
luciferinfery <rudfedor@gmail.com>
2024-01-15 21:38:05 +0200

    HW_31

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const intervalEnd = 100;
+
+const uniqueNumArr = [];
+
+for (let i = 0; i <= intervalEnd; i++) {
+    uniqueNumArr.push(i);
+}
+
+function getRandomUniqueNum() {
+    if (uniqueNumArr.length === 0) {
+        throw new Error('There is no unique values left')
+    }
+    const randomIndex = Math.round(Math.random() * (uniqueNumArr.length - 1));
+    const result = uniqueNumArr[randomIndex];
+    uniqueNumArr.splice(randomIndex, 1);
+    return result;
+}
+
+for (let i = 0; i<= 101; i++){
+    console.log(getRandomUniqueNum());
+}
+
+console.log(uniqueNumArr)

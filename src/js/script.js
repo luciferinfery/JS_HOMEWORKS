b64855d79676f0fe30432fff4636c4d8ad7f925d
luciferinfery <rudfedor@gmail.com>
2024-01-29 20:56:54 +0200

    HW_34

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
-console.log('hello world');
+const composeCreateUser = function () {
+  const greet = function () {
+    console.log(`Hello, my name is ${this.name}`);
+  };
+
+  return function (name, age) {
+    return {
+      name,
+      age,
+      greet,
+    };
+  };
+};
+
+const createUser = composeCreateUser();
+
+const user = createUser('Anna', '24');
+console.log(user);
+user.greet();

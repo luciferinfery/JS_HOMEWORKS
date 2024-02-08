e226563c952a932ba31ac438d7382450d31b36c5
luciferinfery <rudfedor@gmail.com>
2024-02-08 16:56:20 +0200

    HW_38

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+const updateQueryString = (url, key, value) => {
+  const parsedUrl = new URL(url);
+  parsedUrl.searchParams.set(key, value);
+  return parsedUrl.href;
+};
+
+console.log(
+  updateQueryString(
+    'https://example.com/search?q=query&sort=ascending&page=4',
+    'q2',
+    'reftb'
+  )
+);

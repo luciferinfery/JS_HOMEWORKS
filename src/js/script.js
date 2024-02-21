7b790eba4b3a91268195c7a31f853933eb7eb97b
luciferinfery <rudfedor@gmail.com>
2024-02-21 21:48:02 +0200

    HW_45

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+const ladder = {
+  step: 0,
+  up() {
+    this.step += 1;
+    return this;
+  },
+  down() {
+    this.step -= 1;
+    return this;
+  },
+  showStep() {
+    alert(this.step);
+  },
+};
+
+ladder.up().up().down().showStep();

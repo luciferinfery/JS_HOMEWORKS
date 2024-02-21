adf40e64124ee3e9cf5e6431a48f5e142436fc83
luciferinfery <rudfedor@gmail.com>
2024-02-21 22:36:44 +0200

    HW_49

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,26 @@
-console.log('hello world');
+const strArr = ['Cat', 'Dog', 'Lion', 'Bear', 'Deer', 'Elephant'];
+
+const longStrArr = strArr.filter((value) => value.length > 5);
+
+console.log(longStrArr);
+
+const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const ovenSum = numArr.reduce(
+  (accumulator, currentValue) =>
+    currentValue % 2 === 0 ? currentValue + accumulator : accumulator,
+  0
+);
+
+console.log(ovenSum);
+
+const avg =
+  numArr.reduce((accumulator, currentValue) => currentValue + accumulator) /
+  numArr.length;
+console.log(avg);
+
+// variant 2
+const avg2 = numArr.reduce(
+  (accumulator, currentValue) => currentValue / numArr.length + accumulator,
+  0
+);
+console.log(avg2);

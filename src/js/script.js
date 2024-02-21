1a27db0a8e04d4719c319e702ca00d9020764e56
luciferinfery <rudfedor@gmail.com>
2024-02-21 18:09:19 +0200

    HW_46

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+const myApply = (func, thisArg, argArr) => {
+  const args = argArr && Array.isArray(argArr) ? argArr : [];
+  const thisObj = thisArg;
+  thisObj.customFunction = func;
+  const result = thisObj.customFunction(...args);
+  delete thisObj.customFunction;
+  return result;
+};
+const person = {
+  name: 'Ivan',
+  age: 18,
+};
+const printName = function () {
+  console.log(this.name);
+};
+printName();
+myApply(printName, person);

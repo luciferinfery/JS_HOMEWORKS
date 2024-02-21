195adc0775270bdd2931426802bacff15d30b492
luciferinfery <rudfedor@gmail.com>
2024-02-21 15:47:07 +0200

    HW_43

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,27 @@
-console.log('hello world');
+const getFactorial = function (num) {
+  if (num === 0) {
+    return 1;
+  }
+  return num * getFactorial(num - 1);
+};
+
+console.log(getFactorial(9));
+
+const pow = (num, degree) => {
+  if (degree === 0) {
+    return 1;
+  }
+  return num * pow(num, degree - 1);
+};
+
+console.log(pow(2, 3));
+
+const plus = (a, b) => {
+  if (a === 0) {
+    return b;
+  }
+  let newB = b;
+  newB += 1;
+  return plus(a - 1, newB);
+};
+console.log(plus(10, 9));

bb6813dfc49253d2c231e514369ce4149402e5fc
luciferinfery <rudfedor@gmail.com>
2024-02-21 17:43:58 +0200

    HW_44

--- src/js/script.js
+++ src/js/script.js
@@ -5,21 +5,22 @@ const flat = (arr, ...rest) => {
     );
   }
   const newArr = [];
-  const flattenArr = (arr) => {
-    for (let i = 0; i < arr.length; i += 1) {
-      if (Array.isArray(arr[i])) {
-        flattenArr(arr[i]);
+  const flattenArr = (arr1) => {
+    for (let i = 0; i < arr1.length; i += 1) {
+      if (Array.isArray(arr1[i])) {
+        flattenArr(arr1[i]);
       } else {
-        newArr.push(arr[i]);
+        newArr.push(arr1[i]);
       }
     }
-    return newArr;
   };
-  return flattenArr(arr);
+  flattenArr(arr);
+  return newArr;
 };
 const complexArray = [
   [1, 2, [3.1, 3.2, [3.21, 3.22]], 4],
   [5, [6, [7, [8, 9]]]],
   [[10, 11], 12, 13],
 ];
+
 console.log(flat(complexArray));

b0201ab59e7629b7a6722a0ba5876edc8c8c2460
luciferinfery <rudfedor@gmail.com>
2024-02-21 23:18:44 +0200

    HW_50

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+const getObjFromJson = (jsonStr) => {
+  try {
+    return JSON.parse(jsonStr);
+  } catch (e) {
+    throw new Error('Invalid JSON string');
+  }
+};
+
+const jsonEx1 = '{"name": "John", "age": 30}';
+const jsonEx2 = '{"name": "John", "age":';
+console.log(getObjFromJson(jsonEx1));
+console.log(getObjFromJson(jsonEx2));

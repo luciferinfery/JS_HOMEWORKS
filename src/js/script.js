c8bccafd713ee64e4c89ac993ca0466dd9e77687
luciferinfery <rudfedor@gmail.com>
2024-02-21 18:37:24 +0200

    HW_47

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,25 @@
-console.log('hello world');
+const myBind = (func, thisArg, ...argArr) => {
+  const myApply = (func1, thisArg1, argArr1) => {
+    const args = argArr1 && Array.isArray(argArr1) ? argArr1 : [];
+    const thisObj = thisArg1;
+    thisObj.customFunction = func1;
+    const result = thisObj.customFunction(...args);
+    delete thisObj.customFunction;
+    return result;
+  };
+
+  return function (...restArgs) {
+    return myApply(func, thisArg, [...argArr, ...restArgs]);
+  };
+};
+
+const person = {
+  name: 'Ivan',
+  age: 18,
+};
+const printName = function () {
+  console.log(this.name);
+};
+
+const newPrintName = myBind(printName, person);
+newPrintName();

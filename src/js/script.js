91578130eefc1583b1ede037565adb939fbbffa8
luciferinfery <rudfedor@gmail.com>
2024-02-21 22:53:50 +0200

    HW_50

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
-console.log('hello world');
+const product1 = {
+  name: 'broom',
+  price: 100,
+};
+const salePrice = (product) => ({
+  ...product,
+  price: product.price * 0.9,
+});
+
+console.log(salePrice(product1));
+
+const studentsArray = [
+  { name: 'Anna', grade: 85 },
+  { name: 'Bob', grade: 91 },
+  { name: 'Carl', grade: 75 },
+  { name: 'Jenny', grade: 95 },
+];
+const getBestStudents = (students) =>
+  students.filter((student) => student.grade > 90);
+
+console.log(getBestStudents(studentsArray));

2013ff87718f64c4f6b031b1bc39f80fcf97266a
luciferinfery <rudfedor@gmail.com>
2024-02-21 22:06:17 +0200

    HW_48

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+const sum = (a, b) => a + b;
+console.log(sum(2, 5));
+
+const length = (text) => text.length;
+console.log(length('blah blah blah'));
+
+const incrementArr = (arr) => arr.map((value) => value + 1);
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(incrementArr(arr));

8c3f88cea29a2b346d82ccccf4f5efef5e9d7089
luciferinfery <rudfedor@gmail.com>
2024-03-05 22:18:36 +0200

    HW_54

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+const generateList = (arr) => {
+  const ul = document.createElement('ul');
+  for (let i = 0; i < arr.length; i += 1) {
+    const li = document.createElement('li');
+    if (Array.isArray(arr[i])) {
+      li.appendChild(generateList(arr[i]));
+    } else {
+      li.textContent = arr[i];
+    }
+    ul.appendChild(li);
+  }
+  return ul;
+};
+
+const array = [1, 2, [1.1, 1.2, 1.3], 3];
+const list = generateList(array);
+document.body.appendChild(list);
+console.log(list);

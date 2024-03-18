6fb79e2f1ce4d6c1212062ed843bf98efe05f88a
luciferinfery <rudfedor@gmail.com>
2024-03-18 21:51:36 +0200

    HW_59

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+const saveData = () => {
+  const textarea = document.querySelector('textarea#input-area');
+  const container = document.querySelector('div#output');
+
+  const newElement = document.createElement('p');
+  newElement.innerHTML = textarea.value;
+  container.prepend(newElement);
+  textarea.value = '';
+};
+document.getElementById('btn').addEventListener('click', saveData);

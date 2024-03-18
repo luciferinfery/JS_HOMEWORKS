a3dadb5eb71cf40ca42b9017ef4395f0309d07b6
luciferinfery <rudfedor@gmail.com>
2024-03-18 20:08:46 +0200

    HW_55

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,42 @@
-console.log('hello world');
+function createTable() {
+  const table = document.createElement('table');
+
+  for (let i = 0; i < 10; i += 1) {
+    const row = table.insertRow();
+    for (let j = 0; j < 10; j += 1) {
+      const cell = row.insertCell();
+      cell.textContent = i * 10 + j + 1;
+    }
+  }
+
+  document.body.appendChild(table);
+}
+
+//
+// function createTableBadOption() {
+//   const table = document.createElement('table');
+//
+//   for (let i = 0; i < 10; i += 1) {
+//     // const row = table.insertRow();
+//     const tr = document.createElement('tr');
+//     table.appendChild(tr);
+//
+//     for (let j = 0; j < 10; j += 1) {
+//       const cell = document.createElement('td');
+//       cell.textContent = i * 10 + j + 1;
+//       tr.appendChild(cell);
+//     }
+//   }
+//
+//   document.body.appendChild(table);
+// }
+//
+// const timeStart1 = performance.now();
+// createTableBadOption();
+//
+// console.log('Time spent 1', performance.now() - timeStart1);
+//
+// const timeStart2 = performance.now();
+createTable();
+//
+// console.log('Time spent 2', performance.now() - timeStart2);

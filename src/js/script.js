d9bfef9d623ebf53e782bac71b9695319d4796a8
luciferinfery <rudfedor@gmail.com>
2024-03-18 20:47:25 +0200

    HW_58

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+const checkNumber = () => {
+  const input = +document.getElementById('textInput').value;
+  if (Number.isNaN(input)) {
+    alert('Введіть число!');
+  } else {
+    alert(input % 2 === 0 ? 'Парне число' : 'Непарне число');
+  }
+  console.log(input);
+};

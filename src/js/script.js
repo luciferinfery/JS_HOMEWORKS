2569790d8d6121342d6eb398c8549c091a3381fa
luciferinfery <rudfedor@gmail.com>
2024-03-18 23:40:45 +0200

    HW_60

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,26 @@
-console.log('hello world');
+const validate = (event) => {
+  event.preventDefault();
+  const inpData = Array.from(
+    document.querySelectorAll(
+      'input[type=text], input[type=email], input[type=password]'
+    )
+  ).reduce((accum, item) => {
+    accum[item.type] = item.value;
+    return accum;
+  }, {});
+
+  const message = document.createElement('div');
+  message.className = 'message-box';
+
+  if (inpData.text && inpData.email && inpData.password) {
+    message.innerHTML = 'Реєстрація успішна';
+    message.className += ' validation-success';
+  } else {
+    message.innerHTML = 'Будь ласка, заповніть усі поля';
+    message.className += ' validation-error';
+  }
+
+  document.querySelector('div.message-container').append(message);
+};
+
+document.querySelector('button').addEventListener('click', validate);
